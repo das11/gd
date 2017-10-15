81e532ec252d72d835ec0631eb73b65fea318228
das11 <kabird1196@gmail.com>
2017-10-15 18:12:56 +0530

    #2

---
+++ js/init.js
@@ -0,0 +1,25 @@
+// var clock = $('.clock').FlipClock(new Date("February 5, 2016 10:00:00"),{
+//     clockFace: 'DailyCounter',
+//     countdown: true
+//     });  
+
+var clock;
+
+$(document).ready(function() {
+	var clock;
+
+	clock = $('.clock').FlipClock({
+        clockFace: 'DailyCounter',
+        autoStart: false,
+        callbacks: {
+        	stop: function() {
+        		$('.message').html('The clock has stopped!')
+        	}
+        }
+    });
+		    
+    clock.setTime(8288580);
+    clock.setCountdown(true);
+    clock.start();
+
+});
